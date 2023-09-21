cc5b2ce7a6522e0dd610b2227e5d788cd64fe44c
summer030 <karaage4projects@outlook.com>
2023-09-21 15:24:20 +0800

    feat: initial modal ui

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
   const profile = await initialProfile();
@@ -18,12 +19,7 @@ const SetupPage = async () => {
   if (server) {
     return redirect(`/server/${server.id}`);
   }
-  return (
-    <div>
-      Create a Server
-      <div></div>
-    </div>
-  );
+  return <InitialModal />;
 };
 
 export default SetupPage;
