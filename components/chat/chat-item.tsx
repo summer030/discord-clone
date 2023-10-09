99b97bc37400d621d4a0cfab930f0b4c86584e9f
summer030 <karaage4projects@outlook.com>
2023-10-09 16:45:03 +0800

    feat: chat socket hook

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { useRouter, useParams } from "next/navigation";
 import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from "lucide-react";
 import axios from "axios";
 import qs from "query-string";
@@ -57,6 +58,16 @@ export const ChatItem = ({
 }: ChatItemProps) => {
   const [isEditing, setIsEditing] = useState(false);
   const { onOpen } = useModal();
+  const params = useParams();
+  const router = useRouter();
+
+  const onMemberClick = () => {
+    if (member.id === currentMember.id) {
+      return;
+    }
+
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+  };
 
   useEffect(() => {
     const handleKeyDown = (event: any) => {
@@ -114,17 +125,25 @@ export const ChatItem = ({
   return (
     <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
       <div className="group flex gap-x-2 items-start w-full">
-        <div className="cursor-pointer hover:drop-shadow-md transition">
+        <div
+          onClick={onMemberClick}
+          className="cursor-pointer hover:drop-shadow-md transition"
+        >
           <UserAvatar src={member.profile.imageUrl} />
         </div>
         <div className="flex flex-col w-full">
-          <div className="flex items-center">
-            <p className="font-semibold text-sm hover:underline cursor-pointer">
-              {member.profile.name}
-            </p>
-            <ActionTooltip label={member.role}>
-              {roleIconMap[member.role]}
-            </ActionTooltip>
+          <div className="flex items-center gap-x-2">
+            <div className="flex items-center">
+              <p
+                onClick={onMemberClick}
+                className="font-semibold text-sm hover:underline cursor-pointer"
+              >
+                {member.profile.name}
+              </p>
+              <ActionTooltip label={member.role}>
+                {roleIconMap[member.role]}
+              </ActionTooltip>
+            </div>
             <span className="text-xs text-zinc-500 dark:text-zinc-400">
               {timestamp}
             </span>
